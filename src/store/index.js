7201301efc455b8974cdbb5b86168f0d1fba96a5
Danish Ali <danish@codendevs.com>
2021-04-15 16:26:35 +0500

    init commit

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,30 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: {},
+    kumatti: []
   },
   mutations: {
+    addUser(state, payload) {
+      state.user = payload;
+    },
+    addKumatti(state, payload) {
+      state.kumatti.push(payload);
+    }
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    getCurrentUser: state => {
+      return state.user;
+    },
+    getAllKumatti: state => {
+      return state.kumatti;
+    },
+    getKumattiById: (state, payload) => {
+      return state.kumatti.find(x => x.id === payload);
+    }
   }
 })
